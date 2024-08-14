d987436125177c9acb9868ee34155daf00ec3da0
Jye Harry <jyeharry21@hotmail.com>
2024-08-15 00:08:14 +1000

    Update Modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react'
-import { StyleProp, View, ViewStyle } from 'react-native'
+import { ScrollView, StyleProp, View, ViewStyle } from 'react-native'
 import ReactNativeModal, { ModalProps } from 'react-native-modal'
+import { Text } from './Text'
+import { IconButton } from '@/components/IconButton'
 import { theme } from '@/constants/theme'
 import { ChooseRequired } from '@/types'
 
@@ -8,6 +10,11 @@ export const Modal: FC<
   ChooseRequired<
     Partial<ModalProps> & {
       containerStyle?: StyleProp<ViewStyle>
+      header?: React.ReactNode
+      hasHeader?: boolean
+      title?: string
+      headerRightComponent?: React.ReactElement
+      setIsVisible: (v: boolean) => void
     },
     'children' | 'isVisible'
   >
@@ -18,11 +25,44 @@ export const Modal: FC<
   animationIn = 'fadeIn',
   animationOut = 'fadeOut',
   containerStyle,
+  header,
+  hasHeader = true,
+  title,
+  headerRightComponent,
+  setIsVisible,
   ...props
 }) => {
+  const Header = () => (
+    <View
+      style={{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: 16,
+      }}
+    >
+      <IconButton
+        name="close"
+        onPress={() => setIsVisible(false)}
+        style={{ marginLeft: 16, zIndex: 1 }}
+      />
+      <View
+        style={{
+          position: 'absolute',
+          flexDirection: 'row',
+          justifyContent: 'center',
+          width: '100%',
+        }}
+      >
+        <Text>{title}</Text>
+      </View>
+      <View style={{ marginRight: 16 }}>{headerRightComponent}</View>
+    </View>
+  )
   return (
     <ReactNativeModal
       backdropOpacity={backdropOpacity}
+      onBackdropPress={() => setIsVisible(false)}
       animationIn={animationIn}
       animationOut={animationOut}
       animationOutTiming={animationOutTiming}
@@ -33,12 +73,13 @@ export const Modal: FC<
           {
             borderRadius: 8,
             backgroundColor: theme.backgroundColour,
-            height: '77%',
+            maxHeight: '80%',
           },
           containerStyle,
         ]}
       >
-        {children}
+        {hasHeader && <Header />}
+        <ScrollView>{children}</ScrollView>
       </View>
     </ReactNativeModal>
   )
